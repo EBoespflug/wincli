e586fc4effd31d82ff9a42e95af83b6b3e3ed665
Etienne Boespflug <etienne.boespflug@gmail.com>
2018-05-13 13:18:08 +0200

    Update color_list.cpp
    Format.

--- examples/color_list.cpp
+++ examples/color_list.cpp
@@ -5,15 +5,15 @@
 
 int main(int argc, char* argv[])
 {
-	SetConsoleTitle("Color list");
-
-	for (int s = (int)wc::color::black; s <= (int)wc::color::white; ++s) {
-		wc::set_color(std::cout, (wc::color)s, wc::color::black);
-		std::cout << "color " << (int)s << "\n";
-	}
+    SetConsoleTitle("Color list");
 
     for (int s = (int)wc::color::black; s <= (int)wc::color::white; ++s) {
-        wc::set_color(std::cout, wc::color::black, static_cast<wc::color>(s));
+        wc::set_color(std::cout, (wc::color)s, wc::color::black);
+        std::cout << "color " << (int)s << "\n";
+    }
+
+    for (int s = (int)wc::color::black; s <= (int)wc::color::white; ++s) {
+        wc::set_color(std::cout, wc::color::black, (wc::color)s);
         std::cout << "color " << (int)s << "\n";
     }
 
